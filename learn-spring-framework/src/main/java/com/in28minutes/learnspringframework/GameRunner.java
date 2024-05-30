11bbbf8617165351f57a1ac0631000f91d059e53
Maitri Bhagat <maitri26july@gmail.com>
2024-05-30 21:02:10 +0530

    Initial Commit

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/GameRunner.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GamingConsole;
+
+public class GameRunner {
+    private GamingConsole game;
+    
+    public GameRunner(GamingConsole game2) {
+        this.game = game2;
+    }
+    
+    public void run() {
+        System.out.println("Running game: " + game);
+        game.up();
+        game.down();
+        game.left();
+        game.right();
+    }
+}
